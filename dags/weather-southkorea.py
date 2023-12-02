40d1edc02a4bdb6f54a1692b3f5ca5cc3534a476
ssup2 <supsup5642@gmail.com>
2023-12-02 22:44:27 +0900

    Update dag

--- dags/weather-southkorea.py
+++ dags/weather-southkorea.py
@@ -11,7 +11,6 @@ from kubernetes.client import models as k8s_models
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2023, 1, 1, tzinfo=pendulum.timezone("Asia/Seoul")),
     "retries": 5,
     "retry_delay": timedelta(minutes=10),
 }
@@ -20,7 +19,8 @@ default_args = {
 dag = DAG(
     dag_id="weather-southkorea",
     default_args=default_args,
-    schedule=timedelta(hours=1),
+    start_date=datetime(2023, 1, 1, tzinfo=pendulum.timezone("Asia/Seoul")),
+    schedule="@hourly",
     catchup=True,
     max_active_tasks=4,
 )
